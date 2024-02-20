14cb0b3f1578bcc89683bf938a63b2114bba314e
Dhanish Kumar <dhanish@mobiux.in>
2024-02-20 12:51:11 +0530

    Feature: Add a tile map and add pan and zoom feature

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # reseacue
 
 A new Flutter project.
-# reseacue
+
+Developed using Flutter version `3.19.0`
+
+Rules:
+
+1. The generated `.tmx` using `Tiled` software, generates a `<tile>.tsx` file and `<map>.tmx` file. So replace the `tileset` tag in `<map>.tmx` with the one you find in the `<tile>.tsx` file.
--- lib/game.dart
+++ lib/game.dart
@@ -1,3 +1,59 @@
-import 'package:flame/game.dart';
+import 'dart:async';
 
-class Reseacue extends FlameGame {}
+import 'package:flame/components.dart';
+import 'package:flame/events.dart';
+import 'package:flame/game.dart';
+import 'package:flame_tiled/flame_tiled.dart';
+
+class Reseacue extends FlameGame with ScaleDetector {
+  late TiledComponent mapComponent;
+
+  @override
+  Future<void> onLoad() async {
+    camera.viewfinder
+      // Set 0.4 as default zoom level as this zoom level covers the entire map
+      ..zoom = 0.4
+      // Set camera's anchor point to top left of the screen
+      ..anchor = Anchor.topLeft;
+
+    // Load the tile map
+    mapComponent = await TiledComponent.load(
+      // Make sure to add the map location in pubspec.yaml
+      'map.tmx',
+      // This is the size of the indivudual tile in the map, which is 16px
+      Vector2.all(16),
+    );
+
+    // Adding the map component to the world so that the camera finds the map
+    // ( Camera can see and relate to the objects available in the world )
+    world.add(mapComponent);
+  }
+
+  void clampZoom() {
+    // Clamp the minimum and maximum zoom levels
+    camera.viewfinder.zoom = camera.viewfinder.zoom.clamp(0.4, 5.0);
+  }
+
+  late double startZoom;
+
+  @override
+  void onScaleStart(info) {
+    startZoom = camera.viewfinder.zoom;
+  }
+
+  @override
+  void onScaleUpdate(ScaleUpdateInfo info) {
+    final currentScale = info.scale.global;
+
+    // Check if gesture is zoom or pan
+    if (!currentScale.isIdentity()) {
+      camera.viewfinder.zoom = startZoom * currentScale.y;
+      clampZoom();
+    } else {
+      final delta = info.delta.global;
+
+      // Normalizing the delta by dividing the pan change with zoom level
+      camera.viewfinder.position -= (delta / camera.viewfinder.zoom);
+    }
+  }
+}
