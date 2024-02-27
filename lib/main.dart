08bac9628fb46d1cdc48c94fb0204d6a92927441
Dhanish Kumar <dhanish@mobiux.in>
2024-02-27 14:10:05 +0530

    Setup: Logger setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,33 @@
+import 'dart:developer';
+
 import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'package:logging/logging.dart';
 import 'package:reseacue/game.dart';
 
+Logger _log = Logger('main.dart');
+
 void main() {
+  // Subscribe to log messages.
+  Logger.root.onRecord.listen((record) {
+    log(
+      record.message,
+      time: record.time,
+      level: record.level.value,
+      name: record.loggerName,
+      zone: record.zone,
+      error: record.error,
+      stackTrace: record.stackTrace,
+    );
+  });
+
   // This is to make sure everything is loaded and initialized before use
   WidgetsFlutterBinding.ensureInitialized();
 
+  _log.info('Going full screen');
   Flame.device.fullScreen();
+  _log.info('Setting portrait only');
   Flame.device.setPortraitUpOnly();
 
   runApp(
