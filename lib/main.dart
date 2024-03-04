83b529455f1ffbe910c292413bf9307038183245
Ambruni L <ambruni@mobiux.in>
2024-03-04 12:54:40 +0530

    Endless scrolling of background using two identical images

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,86 @@
-import 'package:flame/flame.dart';
-import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
-import 'package:reseacue/game.dart';
 
-void main() {
-  // This is to make sure everything is loaded and initialized before use
-  WidgetsFlutterBinding.ensureInitialized();
+void main() => runApp(const MyApp());
 
-  Flame.device.fullScreen();
-  Flame.device.setPortraitUpOnly();
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-  runApp(
-    const GameWidget.controlled(
-      gameFactory: Reseacue.new,
-    ),
-  );
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHome(),
+    );
+  }
+}
+
+class MyHome extends StatefulWidget {
+  const MyHome({super.key});
+
+  @override
+  State<MyHome> createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 5), // Adjust duration as needed
+    );
+    _animation = Tween<double>(begin: 0, end: 1).animate(_controller)
+      ..addListener(() {
+        setState(() {}); // Redraw widget on animation updates
+      })
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          _controller.reset();
+        } else if (status == AnimationStatus.dismissed) {
+          _controller.forward();
+        }
+      });
+    _controller.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          // First Image
+          Positioned(
+            top: -MediaQuery.of(context).size.height * _animation.value,
+            left: 0,
+            child: Image.asset(
+              'assets/images/road.png',
+              fit: BoxFit.fill,
+              height: MediaQuery.of(context).size.height * 2,
+              width: MediaQuery.of(context).size.width,
+            ),
+          ),
+          // Second Image
+          Positioned(
+            top: MediaQuery.of(context).size.height -
+                MediaQuery.of(context).size.height * _animation.value,
+            left: 0,
+            child: Image.asset(
+              'assets/images/road.png',
+              fit: BoxFit.fill,
+              height: MediaQuery.of(context).size.height * 2,
+              width: MediaQuery.of(context).size.width,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 }
