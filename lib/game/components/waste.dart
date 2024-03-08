f884f22c61a34dee65d3185b0af14b1e379ebd2e
Dhanish Kumar <dhanish@mobiux.in>
2024-03-08 14:44:46 +0530

    Drag and drop the trash on the vehicle

--- lib/game/components/waste.dart
+++ lib/game/components/waste.dart
@@ -31,6 +31,16 @@ class Waste extends SpriteAnimationComponent
   bool tapped = false;
   late Vector2 spawnedPosition;
 
+  late double wetWasteLeftBound;
+  late double wetWasteTopBound;
+  late double wetWasteRightBound;
+  late double wetWasteBottomBound;
+
+  late double dryWasteLeftBound;
+  late double dryWasteTopBound;
+  late double dryWasteRightBound;
+  late double dryWasteBottomBound;
+
   @override
   FutureOr<void> onLoad() {
     spawnedPosition = position.clone();
@@ -42,6 +52,26 @@ class Waste extends SpriteAnimationComponent
     );
 
     anchor = Anchor.center;
+
+    wetWasteLeftBound = game.vehicle.width -
+        (game.vehicle.width * 0.15) -
+        (game.vehicle.width * 0.41 / 2);
+    wetWasteBottomBound = game.vehicle.y - (game.vehicle.height * 0.38);
+    wetWasteRightBound = game.vehicle.x -
+        (game.vehicle.width / 2) +
+        (game.vehicle.width * 0.41 / 2);
+    wetWasteTopBound =
+        game.vehicle.y - game.vehicle.height + (game.vehicle.height * 0.36);
+
+    dryWasteLeftBound = game.vehicle.width -
+        (game.vehicle.width * 0.15) -
+        (game.vehicle.width * 0.41 / 2);
+    dryWasteBottomBound = game.vehicle.y - (game.vehicle.height * 0.09);
+    dryWasteRightBound = game.vehicle.x -
+        (game.vehicle.width / 2) +
+        (game.vehicle.width * 0.41 / 2);
+    dryWasteTopBound =
+        game.vehicle.y - game.vehicle.height + (game.vehicle.height * 0.66);
   }
 
   @override
@@ -61,16 +91,45 @@ class Waste extends SpriteAnimationComponent
   void onDragEnd(DragEndEvent event) {
     _log.info('Drag ended');
     tapped = false;
-    // TODO: Drop logic yet to be added
-    add(
-      MoveEffect.to(
-        spawnedPosition,
-        EffectController(
-          duration: Constants.wasteRepositionAnimationSpeed,
-          curve: Curves.easeIn,
-        ),
-      ),
-    );
+
+    if (type == WasteType.wet) {
+      if ((position.x - width) <
+              wetWasteRightBound && // This is because the anchor point of waste is `Anchor.center`, so the delta `width` corrects that
+          position.y > wetWasteTopBound &&
+          position.x > wetWasteLeftBound &&
+          position.y < wetWasteBottomBound) {
+        removeFromParent();
+      } else {
+        add(
+          MoveEffect.to(
+            spawnedPosition,
+            EffectController(
+              duration: Constants.wasteRepositionAnimationSpeed,
+              curve: Curves.easeIn,
+            ),
+          ),
+        );
+      }
+    } else {
+      if ((position.x - width) <
+              dryWasteRightBound && // This is because the anchor point of waste is `Anchor.center`, so the delta `width` corrects that
+          position.y > dryWasteTopBound &&
+          position.x > dryWasteLeftBound &&
+          position.y < dryWasteBottomBound) {
+        removeFromParent();
+      } else {
+        add(
+          MoveEffect.to(
+            spawnedPosition,
+            EffectController(
+              duration: Constants.wasteRepositionAnimationSpeed,
+              curve: Curves.easeIn,
+            ),
+          ),
+        );
+      }
+    }
+
     super.onDragEnd(event);
   }
 
