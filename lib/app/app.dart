9dd1f93a8627fb85778d76ee31be4788df147e9e
Dhanish Kumar <dhanish@mobiux.in>
2024-03-11 22:56:14 +0530

    Fix loading white screen of game widget

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
@@ -6,12 +8,15 @@ import 'package:reseacue/app/app_lifecycle/app_lifecycle.dart';
 import 'package:reseacue/app/audio/audio_controller.dart';
 import 'package:reseacue/app/settings/persistence/settings_persistence.dart';
 import 'package:reseacue/app/settings/settings.dart';
+import 'package:reseacue/assets.dart';
 import 'package:reseacue/game/game.dart';
 import 'package:reseacue/overlays/overlays.dart';
 import 'package:reseacue/app/ui/splash_screen.dart';
 import 'package:reseacue/constants/constants.dart';
+import 'package:reseacue/utils/images.dart';
+import 'package:transparent_image/transparent_image.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   final SettingsPersistence settingsPersistence;
 
   const App({
@@ -19,62 +24,151 @@ class App extends StatelessWidget {
     super.key,
   });
 
-  static final _router = GoRouter(
-    routes: [
-      GoRoute(
-        path: Path.root,
-        builder: (context, state) => const SplashScreen(
-          key: Key(Constants.splashScreenKey),
-        ),
-        routes: const [],
-      ),
-      GoRoute(
-        path: Path.startGameOverlay,
-        pageBuilder: (context, state) => CustomTransitionPage<void>(
-          key: state.pageKey,
-          child: GameWidget<Reseacue>.controlled(
-            gameFactory: Reseacue.new,
-            overlayBuilderMap: {
-              TutorialOverlay.id: (_, game) => TutorialOverlay(game: game),
-              StartGameOverlay.id: (_, game) => StartGameOverlay(game: game),
-              CountDownOverlay.id: (_, game) => CountDownOverlay(game: game),
-              GamePlayOverlay.id: (BuildContext context, Reseacue game) =>
-                  GamePlayOverlay(
-                    game: game,
-                  ),
-              PauseOverlay.id: (BuildContext context, Reseacue game) =>
-                  PauseOverlay(
-                    game: game,
-                  ),
-              SettingsOverlay.id: (_, game) => SettingsOverlay(game: game),
-              ResetSettingsOverlay.id: (_, game) =>
-                  ResetSettingsOverlay(game: game),
-              GameOverOverlay.id: (_, game) => GameOverOverlay(game: game),
-              ExitConfirmationOverlay.id: (_, game) =>
-                  ExitConfirmationOverlay(game: game),
-            },
-            initialActiveOverlays: [
-              StartGameOverlay.id,
-              TutorialOverlay.id,
-            ],
-          ),
-          transitionsBuilder: (context, animation, secondaryAnimation, child) =>
-              FadeTransition(opacity: animation, child: child),
-        ),
-        routes: const [],
-      ),
-    ],
-  );
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  late Timer timer;
+
+  final values = [];
+  final ValueNotifier<int> _index = ValueNotifier<int>(0);
+
+  @override
+  void initState() {
+    super.initState();
+    for (int treeAnimationFrameIndex = 1;
+        treeAnimationFrameIndex <= 45;
+        treeAnimationFrameIndex++) {
+      values.add(treeAnimationFrameIndex.toString());
+    }
+    timer = Timer.periodic(const Duration(milliseconds: 200), (timer) {
+      _index.value += 1;
+    });
+  }
+
+  @override
+  void dispose() {
+    timer.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
+    final router = GoRouter(
+      routes: [
+        GoRoute(
+          path: Path.root,
+          builder: (context, state) => const SplashScreen(
+            key: Key(Constants.splashScreenKey),
+          ),
+          routes: const [],
+        ),
+        GoRoute(
+          path: Path.startGameOverlay,
+          pageBuilder: (context, state) => CustomTransitionPage<void>(
+            key: state.pageKey,
+            child: GameWidget<Reseacue>.controlled(
+              gameFactory: Reseacue.new,
+              overlayBuilderMap: {
+                TutorialOverlay.id: (_, game) => TutorialOverlay(game: game),
+                StartGameOverlay.id: (_, game) => StartGameOverlay(game: game),
+                CountDownOverlay.id: (_, game) => CountDownOverlay(game: game),
+                GamePlayOverlay.id: (BuildContext context, Reseacue game) =>
+                    GamePlayOverlay(
+                      game: game,
+                    ),
+                PauseOverlay.id: (BuildContext context, Reseacue game) =>
+                    PauseOverlay(
+                      game: game,
+                    ),
+                SettingsOverlay.id: (_, game) => SettingsOverlay(game: game),
+                ResetSettingsOverlay.id: (_, game) =>
+                    ResetSettingsOverlay(game: game),
+                GameOverOverlay.id: (_, game) => GameOverOverlay(game: game),
+                ExitConfirmationOverlay.id: (_, game) =>
+                    ExitConfirmationOverlay(game: game),
+              },
+              initialActiveOverlays: [
+                StartGameOverlay.id,
+                TutorialOverlay.id,
+              ],
+              loadingBuilder: (context) {
+                return Container(
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                        Color(0xFF51B937),
+                        Color(0xFFCBDE81),
+                      ],
+                    ),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      FadeInImage(
+                        placeholder: MemoryImage(kTransparentImage),
+                        image: AssetImage(getPathFromAssetString(
+                            AssetConstants.splashLoading)),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 40),
+                        child: Column(
+                          children: [
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(left: 20, right: 20),
+                              child: ValueListenableBuilder(
+                                valueListenable: _index,
+                                builder: (context, data, child) {
+                                  return FadeInImage(
+                                    placeholder: MemoryImage(kTransparentImage),
+                                    image: AssetImage(
+                                      getPathFromAssetString(
+                                          'Tree animation_${values[data % values.length]}.png'),
+                                    ),
+                                  );
+                                },
+                              ),
+                            ),
+                            const Text(
+                              'Loading...',
+                              style: TextStyle(
+                                fontSize: 30,
+                                color: Colors.white,
+                                fontFamily: 'Digitalt',
+                                fontWeight: FontWeight.w500,
+                                decoration: TextDecoration.none,
+                              ),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                );
+              },
+            ),
+            transitionsBuilder:
+                (context, animation, secondaryAnimation, child) =>
+                    FadeTransition(opacity: animation, child: child),
+          ),
+          routes: const [],
+        ),
+      ],
+    );
+
     return AppLifecycleObserver(
       child: MultiProvider(
         providers: [
           Provider<SettingsController>(
             lazy: false,
             create: (context) => SettingsController(
-              persistence: settingsPersistence,
+              persistence: widget.settingsPersistence,
             )..loadStateFromPersistence(),
           ),
           ProxyProvider2<SettingsController, ValueNotifier<AppLifecycleState>,
@@ -96,9 +190,9 @@ class App extends StatelessWidget {
         child: Builder(builder: (context) {
           return MaterialApp.router(
             debugShowCheckedModeBanner: false,
-            routeInformationProvider: _router.routeInformationProvider,
-            routeInformationParser: _router.routeInformationParser,
-            routerDelegate: _router.routerDelegate,
+            routeInformationProvider: router.routeInformationProvider,
+            routeInformationParser: router.routeInformationParser,
+            routerDelegate: router.routerDelegate,
           );
         }),
       ),
--- lib/app/ui/splash_screen.dart
+++ lib/app/ui/splash_screen.dart
@@ -5,36 +5,9 @@ import 'package:go_router/go_router.dart';
 import 'package:reseacue/assets.dart';
 import 'package:reseacue/constants/constants.dart';
 import 'package:reseacue/utils/images.dart';
-import 'package:transparent_image/transparent_image.dart';
 
-class SplashScreen extends StatefulWidget {
+class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
-
-  @override
-  State<SplashScreen> createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-  late Timer timer;
-
-  final values = [];
-  ValueNotifier<int> _index = ValueNotifier<int>(0);
-
-  @override
-  void initState() {
-    super.initState();
-    for (int treeAnimationFrameIndex = 1;
-        treeAnimationFrameIndex <= 45;
-        treeAnimationFrameIndex++) {
-      values.add(treeAnimationFrameIndex.toString());
-    }
-  }
-
-  @override
-  void dispose() {
-    timer.cancel();
-    super.dispose();
-  }
 
   Future<bool> preloadImages(context) async {
     for (String asset in AssetConstants.assets) {
@@ -51,72 +24,10 @@ class _SplashScreenState extends State<SplashScreen> {
           future: preloadImages(context),
           builder: (context, AsyncSnapshot<bool> snapshot) {
             if (snapshot.data == true) {
-              timer =
-                  Timer.periodic(const Duration(milliseconds: 300), (timer) {
-                _index.value += 1;
-                if (_index.value == 45) {
-                  WidgetsBinding.instance.addPostFrameCallback(
-                    (_) {
-                      GoRouter.of(context).replace(Path.startGameOverlay);
-                    },
-                  );
-                }
-              });
-              return Container(
-                height: MediaQuery.of(context).size.height,
-                width: MediaQuery.of(context).size.width,
-                decoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    colors: [
-                      Color(0xFF51B937),
-                      Color(0xFFCBDE81),
-                    ],
-                  ),
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    FadeInImage(
-                      placeholder: MemoryImage(kTransparentImage),
-                      image: AssetImage(
-                          getPathFromAssetString(AssetConstants.splashLoading)),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 40),
-                      child: Column(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(left: 20, right: 20),
-                            child: ValueListenableBuilder(
-                              valueListenable: _index,
-                              builder: (context, data, child) {
-                                return FadeInImage(
-                                  placeholder: MemoryImage(kTransparentImage),
-                                  image: AssetImage(
-                                    getPathFromAssetString(
-                                        'Tree animation_${values[data % values.length]}.png'),
-                                  ),
-                                );
-                              },
-                            ),
-                          ),
-                          const Text(
-                            'Loading...',
-                            style: TextStyle(
-                              fontSize: 30,
-                              color: Colors.white,
-                              fontFamily: 'Digitalt',
-                              fontWeight: FontWeight.w500,
-                              decoration: TextDecoration.none,
-                            ),
-                          )
-                        ],
-                      ),
-                    )
-                  ],
-                ),
+              WidgetsBinding.instance.addPostFrameCallback(
+                (_) {
+                  GoRouter.of(context).replace(Path.startGameOverlay);
+                },
               );
             }
             return Container(
