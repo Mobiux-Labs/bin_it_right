5ed68dd99aabdfe90262b39bd6adc674d4889b25
Dhanish Kumar <dhanish@mobiux.in>
2024-03-11 22:40:02 +0530

    Update splash screen

--- lib/assets.dart
+++ lib/assets.dart
@@ -93,6 +93,102 @@ class AssetConstants {
   static const String tutorialFifth = 'tutorial_5.png';
   static const String tutorialSixth = 'tutorial_6.png';
 
+  static const String treeAnimationFirstFrame = 'Tree animation_1.png';
+  static const String treeAnimationSecondFrame = 'Tree animation_2.png';
+  static const String treeAnimationThirdFrame = 'Tree animation_3.png';
+  static const String treeAnimationFourthFrame = 'Tree animation_4.png';
+  static const String treeAnimationFifthFrame = 'Tree animation_5.png';
+  static const String treeAnimationSixthFrame = 'Tree animation_6.png';
+  static const String treeAnimationSeventhFrame = 'Tree animation_7.png';
+  static const String treeAnimationEighthFrame = 'Tree animation_8.png';
+  static const String treeAnimationNinthFrame = 'Tree animation_9.png';
+  static const String treeAnimationTenthFrame = 'Tree animation_10.png';
+  static const String treeAnimationEleventhFrame = 'Tree animation_11.png';
+  static const String treeAnimationTwelfthFrame = 'Tree animation_12.png';
+  static const String treeAnimationThirteenthFrame = 'Tree animation_13.png';
+  static const String treeAnimationFourteenthFrame = 'Tree animation_14.png';
+  static const String treeAnimationFifteenthFrame = 'Tree animation_15.png';
+  static const String treeAnimationSixteenthFrame = 'Tree animation_16.png';
+  static const String treeAnimationSeventeenthFrame = 'Tree animation_17.png';
+  static const String treeAnimationEighteenthFrame = 'Tree animation_18.png';
+  static const String treeAnimationNineteenthFrame = 'Tree animation_19.png';
+  static const String treeAnimationTwentiethFrame = 'Tree animation_20.png';
+  static const String treeAnimationTwentyFirstFrame = 'Tree animation_21.png';
+  static const String treeAnimationTwentySecondFrame = 'Tree animation_22.png';
+  static const String treeAnimationTwentyThirdFrame = 'Tree animation_23.png';
+  static const String treeAnimationTwentyFourthFrame = 'Tree animation_24.png';
+  static const String treeAnimationTwentyFifthFrame = 'Tree animation_25.png';
+  static const String treeAnimationTwentySixthFrame = 'Tree animation_26.png';
+  static const String treeAnimationTwentySeventhFrame = 'Tree animation_27.png';
+  static const String treeAnimationTwentyEighthFrame = 'Tree animation_28.png';
+  static const String treeAnimationTwentyNinthFrame = 'Tree animation_29.png';
+  static const String treeAnimationThirtiethFrame = 'Tree animation_30.png';
+  static const String treeAnimationThirtyFirstFrame = 'Tree animation_31.png';
+  static const String treeAnimationThirtySecondFrame = 'Tree animation_32.png';
+  static const String treeAnimationThirtyThirdFrame = 'Tree animation_33.png';
+  static const String treeAnimationThirtyFourthFrame = 'Tree animation_34.png';
+  static const String treeAnimationThirtyFifthFrame = 'Tree animation_35.png';
+  static const String treeAnimationThirtySixthFrame = 'Tree animation_36.png';
+  static const String treeAnimationThirtySeventhFrame = 'Tree animation_37.png';
+  static const String treeAnimationThirtyEighthFrame = 'Tree animation_38.png';
+  static const String treeAnimationThirtyNinthFrame = 'Tree animation_39.png';
+  static const String treeAnimationFortiethFrame = 'Tree animation_40.png';
+  static const String treeAnimationFortyFirstFrame = 'Tree animation_41.png';
+  static const String treeAnimationFortySecondFrame = 'Tree animation_42.png';
+  static const String treeAnimationFortyThirdFrame = 'Tree animation_43.png';
+  static const String treeAnimationFortyFourthFrame = 'Tree animation_44.png';
+  static const String treeAnimationFortyFifthFrame = 'Tree animation_45.png';
+
+  static const String splashLoading = 'splash_loading.png';
+
+  static const List<String> treeAnimationFrames = [
+    treeAnimationFirstFrame,
+    treeAnimationSecondFrame,
+    treeAnimationThirdFrame,
+    treeAnimationFourthFrame,
+    treeAnimationFifthFrame,
+    treeAnimationSixthFrame,
+    treeAnimationSeventhFrame,
+    treeAnimationEighthFrame,
+    treeAnimationNinthFrame,
+    treeAnimationTenthFrame,
+    treeAnimationEleventhFrame,
+    treeAnimationTwelfthFrame,
+    treeAnimationThirteenthFrame,
+    treeAnimationFourteenthFrame,
+    treeAnimationFifteenthFrame,
+    treeAnimationSixteenthFrame,
+    treeAnimationSeventeenthFrame,
+    treeAnimationEighteenthFrame,
+    treeAnimationNineteenthFrame,
+    treeAnimationTwentiethFrame,
+    treeAnimationTwentyFirstFrame,
+    treeAnimationTwentySecondFrame,
+    treeAnimationTwentyThirdFrame,
+    treeAnimationTwentyFourthFrame,
+    treeAnimationTwentyFifthFrame,
+    treeAnimationTwentySixthFrame,
+    treeAnimationTwentySeventhFrame,
+    treeAnimationTwentyEighthFrame,
+    treeAnimationTwentyNinthFrame,
+    treeAnimationThirtiethFrame,
+    treeAnimationThirtyFirstFrame,
+    treeAnimationThirtySecondFrame,
+    treeAnimationThirtyThirdFrame,
+    treeAnimationThirtyFourthFrame,
+    treeAnimationThirtyFifthFrame,
+    treeAnimationThirtySixthFrame,
+    treeAnimationThirtySeventhFrame,
+    treeAnimationThirtyEighthFrame,
+    treeAnimationThirtyNinthFrame,
+    treeAnimationFortiethFrame,
+    treeAnimationFortyFirstFrame,
+    treeAnimationFortySecondFrame,
+    treeAnimationFortyThirdFrame,
+    treeAnimationFortyFourthFrame,
+    treeAnimationFortyFifthFrame
+  ];
+
   static const List<String> assets = [
     splashScreen,
     leftGrass,
@@ -124,5 +220,51 @@ class AssetConstants {
     tutorialFourth,
     tutorialFifth,
     tutorialSixth,
+    treeAnimationFirstFrame,
+    treeAnimationSecondFrame,
+    treeAnimationThirdFrame,
+    treeAnimationFourthFrame,
+    treeAnimationFifthFrame,
+    treeAnimationSixthFrame,
+    treeAnimationSeventhFrame,
+    treeAnimationEighthFrame,
+    treeAnimationNinthFrame,
+    treeAnimationTenthFrame,
+    treeAnimationEleventhFrame,
+    treeAnimationTwelfthFrame,
+    treeAnimationThirteenthFrame,
+    treeAnimationFourteenthFrame,
+    treeAnimationFifteenthFrame,
+    treeAnimationSixteenthFrame,
+    treeAnimationSeventeenthFrame,
+    treeAnimationEighteenthFrame,
+    treeAnimationNineteenthFrame,
+    treeAnimationTwentiethFrame,
+    treeAnimationTwentyFirstFrame,
+    treeAnimationTwentySecondFrame,
+    treeAnimationTwentyThirdFrame,
+    treeAnimationTwentyFourthFrame,
+    treeAnimationTwentyFifthFrame,
+    treeAnimationTwentySixthFrame,
+    treeAnimationTwentySeventhFrame,
+    treeAnimationTwentyEighthFrame,
+    treeAnimationTwentyNinthFrame,
+    treeAnimationThirtiethFrame,
+    treeAnimationThirtyFirstFrame,
+    treeAnimationThirtySecondFrame,
+    treeAnimationThirtyThirdFrame,
+    treeAnimationThirtyFourthFrame,
+    treeAnimationThirtyFifthFrame,
+    treeAnimationThirtySixthFrame,
+    treeAnimationThirtySeventhFrame,
+    treeAnimationThirtyEighthFrame,
+    treeAnimationThirtyNinthFrame,
+    treeAnimationFortiethFrame,
+    treeAnimationFortyFirstFrame,
+    treeAnimationFortySecondFrame,
+    treeAnimationFortyThirdFrame,
+    treeAnimationFortyFourthFrame,
+    treeAnimationFortyFifthFrame,
+    splashLoading,
   ];
 }
